b10fb7435c3bf1abfb69988cd49a8464441d052e
ABHILASH-018 <ninni969594@gmail.com>
2026-07-03 23:07:19 +0530

    commit

---
+++ evenodd.js
@@ -0,0 +1,13 @@
+import{evenOrOdd as ED} from './fxn.js'
+
+let a=9;
+let b=18;
+let c =27;
+let d =36;
+
+console.log(ED(a));
+console.log(ED(b));
+console.log(ED(c));
+console.log(ED(d));
+
+
---
+++ fxn.js
@@ -0,0 +1,10 @@
+export function evenOrOdd(x){
+    if(x%2==0){
+    console.log("the number is even");
+    
+}
+else{
+    console.log("the number is odd");
+    
+}
+}
